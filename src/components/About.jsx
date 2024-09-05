1a19db935a7cf9dd9f688de62410a5408da72ef8
shahiNaresh11 <naresh.shahi78@aadimcollege.edu.np>
2024-09-05 14:36:04 +0545

    add link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ export default function About(){
         <div>
         <h3 className="heading"> few words about aadim</h3>
         <div className="details"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, totam deleniti nostrum, fugit corrupti nemo quaerat praesentium iusto accusantium doloremque deserunt. Ea qui aliquid nostrum veniam dolorem, id laboriosam illo.</div>
+        <a href="">facebook</a>
         </div>
     )
 }
